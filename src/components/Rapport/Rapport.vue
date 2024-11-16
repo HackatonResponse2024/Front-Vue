ad1f61a69ad1b966ca08f4ef32ef0077e8e1589c
Wellan21 <tristan.marcoux21@gmail.com>
2024-11-16 17:27:57 +0100

    remake Gauge

--- src/components/Rapport/Rapport.vue
+++ src/components/Rapport/Rapport.vue
@@ -5,17 +5,21 @@
     
       <div class="row">
         <div class="col card">
-          <GaugeChart />
+          <GaugeChart :title="'Auto Prodution'" 
+          :value="95" />
         </div>
         <div class="col card">
-          <GaugeChart />
+          <GaugeChart :title="'Auto Consomation'" :value="55" />
         </div>
+        <div class="col card">
+          <GaugeChart :title="'Puissance MAx'" :value="996" :unite="' Wh'"/>
+        </div>
+
       </div>
 
   
     <div class="col card">
       <!-- Conteneur pour le graphique MyChartComponent -->
-      
         <MyChartComponent />
     
     </div>
